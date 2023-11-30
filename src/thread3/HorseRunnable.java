d0b7bfd26f7b902eb729e8459c2b9ecffe1b07c9
Dayeongs <ekdud9636@gmail.com>
2023-11-30 14:07:15 +0900

    스레드 제어 실습

---
+++ src/thread3/HorseRunnable.java
@@ -0,0 +1,27 @@
+package thread3;
+
+import java.util.Vector;
+
+public class HorseRunnable implements Runnable {
+	
+	// 순위별로 경주마의 이름을 저장하는 객체
+	private Vector<String> names = new Vector<String>();
+	public Vector<String> getNames() {
+		return names;
+	}
+	
+	@Override
+	public void run() {
+		String name = Thread.currentThread().getName();
+		for(int i = 0; i <= 100; i++) {
+			System.out.println("["+name+"]이 " + i + "미터 전진함...");
+			try {
+				Thread.sleep((int) (Math.random()*10));
+			} catch (Exception ex) {}
+		}
+		
+		System.out.println("["+name+"]이 결승선에 도착함...");
+		names.add(name);
+	}
+
+}
---
+++ src/thread4/Consumer.java
@@ -0,0 +1,22 @@
+package thread4;
+
+// 소비자
+public class Consumer extends Thread {
+
+	private Repo repo;
+	public Consumer(Repo repo) {
+		this.repo = repo;
+	}
+	
+	public void run() {
+		try {
+			for (int i = 0; i < 10; i++) {
+				repo.consume();
+				Thread.sleep(300);
+			}
+		} catch (Exception ex) {
+			
+		}
+	}
+	
+}
